1570aa66fc2fb5a33f4f239146b510273c02e211
IvoyDino <146407385+IvoyDino@users.noreply.github.com>
2024-02-22 23:51:21 -0500

    Update StreamlitChla.py

--- StreamlitChla.py
+++ StreamlitChla.py
@@ -5,15 +5,14 @@ st.set_page_config(page_title="Plant Nutrient Prediction Website", page_icon="ðŸ
 #...........HEADER SECTION.............
 with st.container():
     st.title("Predict Plant Nutrients")
-    st.write("Hello! Thank you for using this web app. This web app is still a prototype, and the nutrient predictions' accuracy will be improved with time. Kindly leave a comment based on your experience and accuracy of the predictions.")
+    st.write("Hello! Thank you for using this web app. This web app is still a prototype under improvement. Kindly leave a comment based on your experience and the accuracy of the predictions.")
     
 #........More Information about using the website..............
 with st.container():
     st.write("---")
-    st.header("How to input your nutrient data")
-    st.write("Input the values of each of the **nutrients in percentage form** and the Chlorophyll concentration will be automatically computed and displayed below under 'Predicted Chlorophyll Content'.")
-    st.write("**NOTE**: for 'Crops' field, the entries should be **whole numbers; 0, 1 and 2 only** as follows: **Corn = 0**; **Sorghum = 1**, and **Soybeans = 2**. So far these are the only plants on which the model was trained.")
-
+    st.header("Plant Chlorophyll prediction")
+    st.write("Input the values of each of the **nutrients in percentage form** and the Chlorophyll concentration will be automatically computed and displayed below as 'Predicted Chlorophyll Content'.")
+    st.write("**NOTE**: for 'Crops' field, the entries should be **0, 1 and 2 only**; for these crops **Corn = 0**; **Sorghum = 1**, and **Soybeans = 2**. So far, these are the only plants on which the model was trained.")
 
 import pandas as pd
 import streamlit as st
@@ -22,10 +21,6 @@ from sklearn.preprocessing import StandardScaler
 import joblib  # For loading the pre-trained model
 import requests
 import os
-
-
-# In[3]:
-
 
 # Load the pre-trained Random Forest model
 model_path = 'Random_Forest_Chl_model.pkl'
@@ -46,7 +41,7 @@ user_input = {}
 for nutrient in nutrient_columns:
     if nutrient == 'Crop':
         # Restrict input values for 'Crop' nutrient to whole numbers between 0 and 2
-        user_input[nutrient] = st.number_input(f'Enter {nutrient} content:', min_value=0, max_value=2, step=1, value=0)
+        user_input[nutrient] = st.number_input(f'Enter {nutrient} value:', min_value=0, max_value=2, step=1, value=0)
     else:
         user_input[nutrient] = st.number_input(f'Enter {nutrient} content (in percentage):', min_value=0.0, max_value=100.0, value=0.0)
 
