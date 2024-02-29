fe6c88f154140277ac96b20c1b07f4c0db9a9843
IvoyDino <146407385+IvoyDino@users.noreply.github.com>
2024-02-28 22:46:11 -0500

    Update StreamlitChla.py

--- StreamlitChla.py
+++ StreamlitChla.py
@@ -24,6 +24,7 @@ def get_user_input(nutrient_columns, suffix=''):
     user_input = {}
     for nutrient in nutrient_columns:
         key = f'{nutrient}_{suffix}' if nutrient != 'Crop' else nutrient
+        key = f'{key}_{suffix}' if key in user_input else key
         if nutrient == 'Crop':
             user_input[nutrient] = st.number_input(f'Enter {nutrient} value:', min_value=0, max_value=2, step=1, value=0, key=key)
         else:
