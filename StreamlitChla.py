99bc2ef10809e16ac02ba0918469b06e1d047e17
IvoyDino <146407385+IvoyDino@users.noreply.github.com>
2024-03-05 23:38:32 -0500

    Update StreamlitChla.py

--- StreamlitChla.py
+++ StreamlitChla.py
@@ -18,7 +18,7 @@ import os
 #Use local css
 def local_css(file_name):
     with open(file_name) as f:
-        st.markdown(f"<style>{f.read()}<style>",unsafe_allow_html=True)
+        st.markdown(f"<style>{f.read()}</style>",unsafe_allow_html=True)
 
 local_css("style.css")
 
