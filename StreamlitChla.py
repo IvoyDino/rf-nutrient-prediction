32301991804a14e56802d1a54dcda0c24e31e344
IvoyDino <146407385+IvoyDino@users.noreply.github.com>
2024-03-05 23:00:57 -0500

    Update StreamlitChla.py

--- StreamlitChla.py
+++ StreamlitChla.py
@@ -203,7 +203,7 @@ contact_form = """
     <input type ="hidden" name="_captcha" value="false">
      <input type="text" name="name" placeholder = "Your name" required>
      <input type="email" name="email" placeholder = "Your email" required>
-     <input type="country" name="country" placeholder = "Your country of location" required>
+     <input type="country" name="country" placeholder = "Country where you are accessing this web app (optional)">
      <textarea name ="message" placeholder = "Your message here" required></textarea>
      <button type="submit">Send</button>
 </form> 
