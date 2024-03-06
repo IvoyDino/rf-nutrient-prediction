7e03cefe454c68df0530b1c1fc05e83206dac50d
IvoyDino <146407385+IvoyDino@users.noreply.github.com>
2024-03-06 00:08:59 -0500

    Update StreamlitChla.py

--- StreamlitChla.py
+++ StreamlitChla.py
@@ -23,7 +23,7 @@ st.markdown(google_analytics_tracking_code(), unsafe_allow_html=True)
 #...........HEADER SECTION.............
 with st.container():
     st.title("Predict Plant Nutrients")
-    st.write("Hello! Thank you for using this web app. This web app is still a prototype under improvement. Kindly leave a comment based on your experience and the accuracy of the predictions.")
+    st.write("Hello! Thank you for using this web app developed by Ivan Oyege. This app is under testing and your feedback will help in improving it. Kindly leave a comment in the 'Contact Me' section at the bottom of the page, based on your experience and the accuracy of the predictions.")
     
 #........More Information about using the website.............. 
 import pandas as pd
@@ -68,21 +68,30 @@ with st.container():
     st.write("Input the values of each of the **nutrients in percentage form** and the Chlorophyll concentration will be automatically computed and displayed below as 'Predicted Chlorophyll Content'.")
     st.write("**NOTE**: for 'Crops' field, the entries should be **0, 1 and 2 only**; for these crops **Corn = 0**; **Sorghum = 1**, and **Soybeans = 2**. So far, these are the only plants on which the model was trained.")
 
+def load_chlorophyll_model():
+    return load_model('Random_Forest_Chl_model.pkl')
+
 # Load Chlorophyll Model
-chlorophyll_model = load_model('Random_Forest_Chl_model.pkl')
-nutrient_columns_chl = ['N', 'P', 'K', 'Ca', 'Mg', 'S', 'Crop']
-user_input_chl = get_user_input(nutrient_columns_chl)
+left_column, right_column = st.columns(2)
+with left_column:
+    chlorophyll_model = load_chlorophyll_model()
+    nutrient_columns_chl = ['N', 'P', 'K', 'Ca', 'Mg', 'S', 'Crop']
+    user_input_chl = get_user_input(nutrient_columns_chl)
 
-# Submit button for Chlorophyll Prediction
-if st.button('Submit for Chlorophyll Prediction'):
-    valid_inputs_chl = all(0.0 <= user_input_chl[nutrient] <= 100.0 for nutrient in nutrient_columns_chl if nutrient != 'Crop')
-    if not valid_inputs_chl:
-        st.error("Please enter nutrient values in percentage form (0 to 100).")
-    else:
-        user_input_df_chl = pd.DataFrame([user_input_chl])
-        prediction_chl = make_prediction(chlorophyll_model, user_input_df_chl)
-        st.write("**The predicted Chlorophyll content is:**", prediction_chl[0])
+    # Submit button for Chlorophyll Prediction
+    if st.button('Submit for Chlorophyll Prediction'):
+        valid_inputs_chl = all(0.0 <= user_input_chl[nutrient] <= 100.0 for nutrient in nutrient_columns_chl if nutrient != 'Crop')
+        if not valid_inputs_chl:
+            st.error("Please enter nutrient values in percentage form (0 to 100).")
+        else:
+            user_input_df_chl = pd.DataFrame([user_input_chl])
+            prediction_chl = make_prediction(chlorophyll_model, user_input_df_chl)
+            st.write("**The predicted Chlorophyll content is:**", prediction_chl[0])
 
+with right_column:
+    st.empty()
+
+    
 # Nitrogen Prediction
 with st.container():
     st.write("---")
@@ -228,7 +237,6 @@ contact_form = """
     <input type ="hidden" name="_captcha" value="false">
      <input type="text" name="Name" placeholder = "Your name" required>
      <input type="email" name="Email" placeholder = "Your email" required>
-     <input type="country" name="Country" placeholder = "Country of accessing">
      <textarea name ="Message" placeholder = "Your message here" required></textarea>
      <button type="submit">Send</button>
 </form> 
